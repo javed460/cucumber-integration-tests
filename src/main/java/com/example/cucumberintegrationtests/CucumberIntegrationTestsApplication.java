fcb07e7e8ed314538f4a072e671d15c41c707394
Javed Bagwan <javed460@gmail.com>
2023-07-24 08:18:32 +0530

    initial project setup

---
+++ src/main/java/com/example/cucumberintegrationtests/CucumberIntegrationTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cucumberintegrationtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberIntegrationTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberIntegrationTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cucumberintegrationtests/CucumberIntegrationTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cucumberintegrationtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberIntegrationTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
